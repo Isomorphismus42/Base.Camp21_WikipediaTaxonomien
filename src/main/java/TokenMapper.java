2f1b23d79495f32eb26233936067c30151381468
okean123 <okean.123@mail.ru>
2021-03-01 18:08:59 +0100

    Added SentenceDetector, Tokenizer and POSTagger

--- src/main/java/TokenMapper.java
+++ src/main/java/TokenMapper.java
@@ -1,8 +1,17 @@
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -13,15 +22,59 @@ public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
     private Text sentence = new Text();
     private Text result = new Text();
     private Analyzer analyzer = new Analyzer();
+    private SentenceDetectorME sentenceDetector;
+    private TokenizerME tokenizer;
+    private POSTaggerME posTagger;
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-       // int z = 0;
-        StringTokenizer itr = new StringTokenizer(value.toString(), "\n\r");
+        // StringTokenizer itr = new StringTokenizer(value.toString(), "\n\r");
+        StringTokenizer itr = new StringTokenizer(value.toString());
 
+        // get sentence model
+        try (InputStream sModelIn = new FileInputStream("en-sent.bin")) {
+            SentenceModel sModel = new SentenceModel(sModelIn);
+            sentenceDetector = new SentenceDetectorME(sModel);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // get tokenizer model
+        try (InputStream tModelIn = new FileInputStream("en-token.bin")) {
+            TokenizerModel tModel = new TokenizerModel(tModelIn);
+            tokenizer = new TokenizerME(tModel);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // get pos tagger model
+        try (InputStream posModelIn = new FileInputStream("en-pos-maxent.bin")) {
+            POSModel posModel = new POSModel(posModelIn);
+            posTagger = new POSTaggerME(posModel);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // TODO Später rausnehmen?
         while (itr.hasMoreTokens()) {
             sentence.set(itr.nextToken());
 
-//            ArrayList<String> groups = analyzer.checkSentence(sentence.toString());
+
+            String sentences[] = sentenceDetector.sentDetect(sentence.toString());
+
+            for (int i = 0; i < sentences.length; i++) {
+                String tokens[] = tokenizer.tokenize(sentences[i]);
+                String tags[] = posTagger.tag(tokens);
+                for (int j = 0; j < tags.length; j++) {
+                     context.write(new Text(tags[j]), one);
+                }
+            }
+
+            //            ArrayList<String> groups = analyzer.checkSentence(sentence.toString());
 
 //            groups.forEach((s) -> {
 
@@ -34,7 +87,6 @@ public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
                     e.printStackTrace();
                 } */
 //            });
-            context.write(sentence, one);
         }
     }
 }
