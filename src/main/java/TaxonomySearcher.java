7ec59546d5d65b684b52c3619d60828ab78a2758
Fabian <9kazemi@informatik.uni-hamburg.de>
2021-03-02 12:33:27 +0100

    Main angepasst, etc.

--- src/main/java/TaxonomySearcher.java
+++ src/main/java/TaxonomySearcher.java
@@ -1,15 +1,28 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class TaxonomySearcher {
+public class TaxonomySearcher extends Configured implements Tool {
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
+        // Let ToolRunner handle generic command-line options
+        int res = ToolRunner.run(new Configuration(), new TaxonomySearcher(), args);
+
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = this.getConf();
+
+        // Create job
         Job job = Job.getInstance(conf, "taxonomysearcher");
         job.setJarByClass(TaxonomySearcher.class);
 
@@ -24,7 +37,7 @@ public class TaxonomySearcher {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 }
 
--- src/main/java/TokenMapper.java
+++ src/main/java/TokenMapper.java
@@ -12,14 +12,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 // Mapper <Input Key, Input Value, Output Key, Output Value>
 public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
 
     private final static IntWritable one = new IntWritable(1);
-    private Text sentence = new Text();
+    private Text line = new Text();
     private Text result = new Text();
     private Analyzer analyzer = new Analyzer();
     private SentenceDetectorME sentenceDetector;
@@ -27,16 +26,14 @@ public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
     private POSTaggerME posTagger;
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        // StringTokenizer itr = new StringTokenizer(value.toString(), "\n\r");
-        StringTokenizer itr = new StringTokenizer(value.toString());
+         StringTokenizer itr = new StringTokenizer(value.toString(), "\n\r");
+//        StringTokenizer itr = new StringTokenizer(value.toString());
 
-        // get sentence model
+        // get line model
         try (InputStream sModelIn = new FileInputStream("en-sent.bin")) {
             SentenceModel sModel = new SentenceModel(sModelIn);
             sentenceDetector = new SentenceDetectorME(sModel);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -46,8 +43,6 @@ public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
             tokenizer = new TokenizerME(tModel);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         // get pos tagger model
@@ -56,25 +51,22 @@ public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
             posTagger = new POSTaggerME(posModel);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
         // TODO Später rausnehmen?
         while (itr.hasMoreTokens()) {
-            sentence.set(itr.nextToken());
+            line.set(itr.nextToken());
+            String[] sentences = sentenceDetector.sentDetect(line.toString());
 
-
-            String sentences[] = sentenceDetector.sentDetect(sentence.toString());
-
-            for (int i = 0; i < sentences.length; i++) {
-                String tokens[] = tokenizer.tokenize(sentences[i]);
-                String tags[] = posTagger.tag(tokens);
-                for (int j = 0; j < tags.length; j++) {
+            for (int i = 0; i < sentences.length ;i++) {
+                String[] tokens = tokenizer.tokenize(sentences[i]);
+                String[] tags = posTagger.tag(tokens);
+                for (int j = 0; j<tags.length; j++) {
                      context.write(new Text(tags[j]), one);
                 }
             }
 
-            //            ArrayList<String> groups = analyzer.checkSentence(sentence.toString());
+//            ArrayList<String> groups = analyzer.checkSentence(line.toString());
 
 //            groups.forEach((s) -> {
 
