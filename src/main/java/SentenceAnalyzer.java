b0378c9600ae8f8cc2fbfa66f25f2719b2554d7c
Fabian <9kazemi@informatik.uni-hamburg.de>
2021-03-04 23:33:20 +0100

    "is/are one of"-Pattern ergänzt Pattern Checks in Methoden ausgegliedert. LowerCase formatierung ergänzt Personalpronomen entfernt

--- src/main/java/SentenceAnalyzer.java
+++ src/main/java/SentenceAnalyzer.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,22 +11,86 @@ public class SentenceAnalyzer {
     private List<String> results;
     private Pattern suchAsPattern;
     private Pattern isOneOfPattern;
-    private  Pattern especiallyPattern;
+    private Pattern especiallyPattern;
+    private Matcher patternMatcher;
 
     public SentenceAnalyzer() {
         initPatterns();
     }
 
     /**
-     * Prüft einen gegebenen getaggten Satz auf Schlüsselwörter wie "such as" oder "is a" und
-     * bildet darus entsprechende Taxonimie-Relationen.
+     * Prüft einen gegebenen getaggten Satz auf Schlüsselwörter wie z.B. "such as" oder "is one of" und
+     * gibt die gefundenen Matches als Array zurück.
      * @param taggedSentence Der zu prüfende getaggte Satz
      * @return ein String Array mit den gefundenen Taxonomie-Relationen im Format VATER#KIND#SchlüsselwortID
      */
     public String[] checkSentence(String taggedSentence) {
         results = new ArrayList<String>();
+        patternMatcher = suchAsPattern.matcher(taggedSentence);
 
-        Matcher patternMatcher = suchAsPattern.matcher(taggedSentence);
+        checkSuchAsPattern();
+
+        checkIsOneOfPattern();
+
+        checkEspeciallyPattern();
+
+        //Gebe alle gefundenen Relationen zurück
+        if (results.isEmpty()){
+            return new String[] {""};
+        }
+        else {
+            return results.toArray(new String[0]);
+        }
+    }
+
+    /**
+     * Prüft den gegeben Satz auf das Key-Wort "especially" und fügt gefundene Matches nach Konvertierung ins
+     * VATER:KIND:2
+     * Format, der Result-Liste hinzu.
+     */
+    private void checkEspeciallyPattern() {
+        patternMatcher.usePattern(especiallyPattern);
+    }
+
+    /**
+     * Prüft den gegeben Satz auf das Key-Wort "is/are one of" und fügt gefundene Matches nach Konvertierung ins
+     * VATER:KIND:2
+     * Format, der Result-Liste hinzu.
+     */
+    private void checkIsOneOfPattern() {
+        patternMatcher.usePattern(isOneOfPattern);
+
+        if (patternMatcher.find()) {
+            //zerlege den gefunden String in Subjekt,Objekt
+            String[] subjectObject = patternMatcher.group().split(" (is_VBZ|are_VBP)_B-VP one_CD_B-NP of_IN_B-PP ");
+
+            //entferne "and"/"or"/"," und splitte dabei
+            String[] subjects = handleAndOrComma(subjectObject[0]);
+            String[] objects = handleAndOrCommaOf(subjectObject[1]);;
+
+            //baut die VATER#KIND-Relation als String und entfernt dabei unerwünschte Wörter und die Tags
+            for (String subject : subjects){
+                for(String object : objects){
+                    subject = removeExpendablyWords(subject);
+                    subject = removeTags(subject);
+                    object = removeExpendablyWords(object);
+                    object = removeTags(object);
+
+                    if (!subject.isEmpty() && !object.isEmpty()) {
+                        results.add(object.toLowerCase(Locale.ROOT) + "#" + subject.toLowerCase(Locale.ROOT) + "#2");
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Prüft den gegeben Satz auf das Key-Wort "such as" und fügt gefundene Matches nach Konvertierung ins
+     * VATER:KIND:2
+     * Format, der Result-Liste hinzu.
+     */
+    private void checkSuchAsPattern() {
+        patternMatcher.usePattern(suchAsPattern);
 
         if (patternMatcher.find()) {
             //zerlege den gefunden String in Subjekt,Objekt
@@ -44,26 +109,14 @@ public class SentenceAnalyzer {
                     object = removeTags(object);
 
                     if (!subject.isEmpty() && !object.isEmpty()) {
-                        results.add(subject + "#" + object + "#1"); //TODO: hänge später #Id zum identifizieren an
+                        results.add(subject.toLowerCase(Locale.ROOT) + "#" + object.toLowerCase(Locale.ROOT) + "#1");
                     }
                 }
             }
         }
-
-        patternMatcher.usePattern(isOneOfPattern);
-
-
-
-        //TODO: weitere Patternprüfungen ergänzen
-
-        //Gebe alle gefundenen Relationen zurück
-        if (results.isEmpty()){
-            return new String[] {""};
-        }
-        else {
-            return results.toArray(new String[0]);
-        }
     }
+
+
 
     /**
      *Entfernt überflüssige, unerwünschte Wörter.
@@ -78,6 +131,8 @@ public class SentenceAnalyzer {
         sentence = sentence.replaceAll("[\\w -]*_RB(R|S)?_(I|B)-NP ?","");
         //remove Comparative adjectives and all previous Words (higher, larger, best, more, earlier, ...)
         sentence = sentence.replaceAll("[\\w -]*_JJ(R|S)_(I|B)-NP ?","");
+        //remove Personal pronoun (he, she, it, his, ...)
+        sentence = sentence.replaceAll("[\\w -]*_PRP\\$?_(I|B)-NP ?", "");
         //remove verbs
         sentence = sentence.replaceAll("[\\w -]*_VBN_(I|B)-NP ?","");
         //remove many,several
@@ -95,6 +150,22 @@ public class SentenceAnalyzer {
         return s.split(" and_CC_I-NP ?| and_CC_O | or_CC_I-NP ?| or_CC_O | ,_,_I-NP | ,_,_O ");
     }
 
+    /**
+     * Zerlegt einen Satzteil in mehrere Teile anhand Trenwörter "and", "or" und ",",
+     * und zusätzlich noch nach dem Trennwort "of".
+     *
+     * @param s der zu zerlegende Satzteil
+     * @return die entstehenden Satz-Fragmente als String-Array
+     */
+    private String[] handleAndOrCommaOf(String s) {
+        return s.split(" and_CC_I-NP ?| and_CC_O | or_CC_I-NP ?| or_CC_O | ,_,_I-NP | ,_,_O | of_IN_B-PP ");
+    }
+
+    /**
+     * Entfernt die Part-of-Speech Tags aller Wörter des gegeben Strings
+     * @param s der Gegebene String
+     * @return der String ohne Wort-Tags
+     */
     private String removeTags(String s){
 //        for (int i=0; i<results.size();i++) {
 //            results.set(i,results.get(i).replaceAll("(_([A-Z]{2,4}|-LRB-))?_(I|B)-NP","").trim());
@@ -113,7 +184,8 @@ public class SentenceAnalyzer {
 
         isOneOfPattern = Pattern.compile("[\\w-]*(_B-NP)( [\\w-]*_I-NP)*" +
                 " (is_VBZ|are_VBP)_B-VP one_CD_B-NP of_IN_B-PP " +
-                "[\\w-]*(_B-NP)( [\\w-]*_I-NP)*");  //Wort_B-NP (ggf weitere Wörter_I-NP)
+                "[\\w-]*(_B-NP)( ([\\w-]*_I-NP|of_IN_B-PP [\\w-]*(_B-NP)|and_CC_O [\\w-]*(_B-NP)|or_CC_O [\\w-]*(_B-NP)|,_,_O [\\w-]*(_B-NP)))*");
+        //Wort_B-NP (ggf weitere Wörter_I-NP auch mit "und", "oder" , "," oder "of" verkettet)
 
         especiallyPattern = Pattern.compile("[\\w-]*(_B-NP)( [\\w-]*_I-NP)*" +
                 " ,_,_O especially_RB_B-ADVP " +
