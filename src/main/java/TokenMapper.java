91bb33d518e3873ead1016b45793d2c8d36208e1
okean123 <okean.123@mail.ru>
2021-03-05 17:04:22 +0100

    Ergebnise erhalten weight abhängig vom Pattern

--- src/main/java/TokenMapper.java
+++ src/main/java/TokenMapper.java
@@ -1,5 +1,7 @@
 import opennlp.tools.chunker.ChunkerME;
 import opennlp.tools.chunker.ChunkerModel;
+import opennlp.tools.lemmatizer.LemmatizerME;
+import opennlp.tools.lemmatizer.LemmatizerModel;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.sentdetect.SentenceDetectorME;
@@ -11,7 +13,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -24,50 +25,18 @@ public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
     private TokenizerME tokenizer;
     private POSTaggerME posTagger;
     private ChunkerME chunker;
+    private LemmatizerME lemmatizer;
 
     public TokenMapper() {
         sentenceAnalyzer = new SentenceAnalyzer();
-        createModels();
+        try {
+            createModels();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void createModels() {
-        // get line model
-        try (InputStream sModelIn = new FileInputStream("en-sent.bin")) {
-            SentenceModel sModel = new SentenceModel(sModelIn);
-            sentenceDetector = new SentenceDetectorME(sModel);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // get tokenizer model
-        try (InputStream tModelIn = new FileInputStream("en-token.bin")) {
-            TokenizerModel tModel = new TokenizerModel(tModelIn);
-            tokenizer = new TokenizerME(tModel);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // get pos tagger model
-        try (InputStream posModelIn = new FileInputStream("en-pos-maxent.bin")) {
-            POSModel posModel = new POSModel(posModelIn);
-            posTagger = new POSTaggerME(posModel);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // get chunker model
-        try (InputStream modelIn = new FileInputStream("en-chunker.bin")){
-            ChunkerModel Cmodel = new ChunkerModel(modelIn);
-            chunker = new ChunkerME(Cmodel);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 //        StringTokenizer itr = new StringTokenizer(value.toString(), "\n\r");
@@ -78,16 +47,69 @@ public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
             String[] tokens = tokenizer.tokenize(sentence);
             String[] tags = posTagger.tag(tokens);
             String[] chunks = chunker.chunk(tokens, tags);
-            //TODO:konvertiere Wöter in Singular mit Lemmatizer?
+//            String[] lemmas = lemmatizer.lemmatize(tokens, tags);
 
             String taggedSentence = "";
             for (int i=0;i< chunks.length;i++) {
+                /*if (tags[i].equals("NN") || tags[i].equals("NNS")) {
+                    tokens[i] = lemmas[i];
+                }*/
                 taggedSentence += tokens[i] + "_" + tags[i] + "_" + chunks[i] + " ";
             }
             String[] results = sentenceAnalyzer.checkSentence(taggedSentence);
+            int weight = 0;
+
             for (String result : results){
-                context.write(new Text(result), one);
+                if (result.length() > 2) {
+                    String tag = result.substring(result.length() - 2).replace("#","");
+                    switch (tag) {
+                        case "1":
+                        case "1a":
+                            weight = 4; break;
+                        case "2":
+                        case "2a":
+                            weight = 3; break;
+                        case "3":
+                        case "3a":
+                            weight = 3; break;
+                        default: break;
+                    }
+                    // Schneidet den Tag vom Ergebnis ab
+                    result= result.substring(0,result.indexOf("#", result.indexOf("#") + 1));
+                }
+
+                context.write(new Text(result), new IntWritable(weight));
             }
+        }
+    }
+
+
+
+
+    private void createModels() throws IOException {
+        // get line model
+        try (InputStream sModelIn = new FileInputStream("en-sent.bin")) {
+            SentenceModel sModel = new SentenceModel(sModelIn);
+            sentenceDetector = new SentenceDetectorME(sModel);
+        }
+        // get tokenizer model
+        try (InputStream tModelIn = new FileInputStream("en-token.bin")) {
+            TokenizerModel tModel = new TokenizerModel(tModelIn);
+            tokenizer = new TokenizerME(tModel);
+        }
+        // get pos tagger model
+        try (InputStream posModelIn = new FileInputStream("en-pos-maxent.bin")) {
+            POSModel posModel = new POSModel(posModelIn);
+            posTagger = new POSTaggerME(posModel);
+        }
+        // get chunker model
+        try (InputStream cModelIn = new FileInputStream("en-chunker.bin")){
+            ChunkerModel Cmodel = new ChunkerModel(cModelIn);
+            chunker = new ChunkerME(Cmodel);
+        }
+        try (InputStream lModelIn = new FileInputStream("en-lemmatizer.bin")) {
+            LemmatizerModel lModel = new LemmatizerModel(lModelIn);
+            lemmatizer = new LemmatizerME(lModel);
         }
     }
 }
