b884abbe5b13112927b25c1bf9583e73558c2db3
Fabian <9kazemi@informatik.uni-hamburg.de>
2021-04-11 21:22:16 +0200

    Initial Commit

---
+++ AdditionalScripts/cleanTaxonomyData.py
@@ -0,0 +1,37 @@
+"""
+Einfaches Script zum entfernen Von Reflexiven und Symmetrischen Relationen,
+sowie möglichen Leereinträgen in den Spalten.
+"""
+
+
+#  Parameter: Verzeichnis der Daten
+def cleanData(datei):
+    summeReflexiv = 0
+    summeSymmetrisch = 0
+    summeLeer = 0
+    menge = set()
+    with open(datei, "r", encoding='UTF-8') as f:
+        taxonomienClean = open(datei[:-4] + "_clean.txt", "x", encoding='UTF-8')
+        ref = open(datei[:-4] + "_entfernteReflexionen.txt", "x", encoding='UTF-8')
+        symm = open(datei[:-4] + "_entfernteSymmetrien.txt", "x", encoding='UTF-8')
+        for line in f:
+            colums = line.lower().split("\t")
+            if (colums[0] == colums[1]):
+                summeReflexiv += 1
+                ref.write(line)
+            elif (colums[0] == "" or colums[1] == ""):
+                summeLeer += 1
+            elif colums[1] + "\t" + colums[0] in menge:
+                summeSymmetrisch += 1
+                symm.write(line)
+            else:
+                menge.add(colums[0] + "\t" + colums[1])
+                taxonomienClean.write(line)
+    print("Reflexiv entfernt: " + str(summeReflexiv))
+    print("Symmetrisch entfernt: " + str(summeSymmetrisch))
+    print("Leereinträge entfernt: " + str(summeLeer))
+
+
+if __name__ == '__main__':
+    datei = 'wiki_all_sorted_final.txt'  # Name/Pfad der zu bereinigen Text-Datei.
+    cleanData(datei)
---
+++ AdditionalScripts/taxonomySample.py
@@ -0,0 +1,28 @@
+"""
+Einfaches Skript zum durchführen Einer Stichporbe auf den Taxonomiedaten.
+"""
+import random
+
+
+# Parameter: Verzeichnis der Daten und Stichprobengröße
+def rateRandomLines(datei, stichprobengroesse):
+    randomlist = []
+    summe = 0
+    num_lines = sum(1 for lines in open(datei))
+    for j in range(0, stichprobengroesse):
+        rdm = random.randint(1, num_lines)
+        randomlist.append(rdm)
+
+    with open(datei, "r", encoding='UTF-8') as f:
+        for i, line in enumerate(f):
+            if i in randomlist:
+                print(f.readline())
+                k = input("Taxonomie (1=ja,0=nein)")
+                while not (k == "1" or k == "0"):
+                    k = input("falsche Eingabe, Eingabe wiederholen:")
+                summe = summe + int(k)
+    print(float(summe) / stichprobengroesse)
+
+
+if __name__ == '__main__':
+    rateRandomLines('wiki_all_sorted_clean.txt', 100)
